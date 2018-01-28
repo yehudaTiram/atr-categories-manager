500e1cfc62c7ce9d9677768a8312c97e912e3fb4
Yehuda Tiram <yehuda@atarimtr.co.il>
2018-01-28 15:30:44 +0200

    added treeview https://www.jstree.com/

--- public/js/atr-categories-manager-public.js
+++ public/js/atr-categories-manager-public.js
@@ -1,8 +1,24 @@
 (function( $ ) {
 	'use strict';
+	// $( document ).ready(function() {
+		// //https://github.com/vakata/jstree
+		// $('.atr-cm-wrap').jstree();
+	// });
 	$( document ).ready(function() {
-		$('.atr-cm-wrap').jstree();
-	});
-	
+		$('.atr-cm-wrap').jstree({
+		  "core" : {
+			"check_callback" : function (operation, node, parent, position, more) {
+			  if(operation === "copy_node" || operation === "move_node") {
+				if(parent.id === "#") {
+				  return false; // prevent moving a child above or below the root
+				}
+			  }
+			  return true; // allow everything else
+			}
+		  },
+		  "plugins" : ["dnd","contextmenu"]
+		});		
+	});	
+
 
 })( jQuery );
